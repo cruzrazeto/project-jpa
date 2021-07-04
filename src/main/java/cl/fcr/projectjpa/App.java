68d8d818de263aeef8de484295ba5b8adaefd3c5
Felipe Cruz <cruzrazeto@gmail.com>
2021-07-03 22:51:08 -0400

    Version inicial

---
+++ src/main/java/cl/fcr/projectjpa/App.java
@@ -0,0 +1,13 @@
+package cl.fcr.projectjpa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
